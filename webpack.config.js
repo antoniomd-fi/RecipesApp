e1c8a59901456e0c8375da0ffd0e9982b8b3e5f6
Victor Sosa <manuelsosapia@gmail.com>
2022-08-15 23:37:10 -0500

    added bootstrap and webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -2,47 +2,47 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    // configuracion de entrada y salida del bundler
-    entry: {
-        bundle: './src/js/index.js'
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].js'
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Webpack App',
-            filename: 'index.html',
-            template: './src/index.html'
-        })
-    ],
-    // Estilos para la pagina
-    module: {
-        rules: [
-            { test: /\.css$/, use: ['style-loader', 'css-loader'] },
-            {
-              test: /\.m?js$/,
-              exclude: /node_modules/,
-              use: {
-                loader: "babel-loader",
-                options: {
-                  presets: ['@babel/preset-env']
-                }
-              }
-            },
-            {
-              test: /\.(png|svg|jpg|jpeg|gif)$/i, 
-              type: 'asset/resource',
-            },
-            {
-              test: /\.html$/i,
-              loader: 'html-loader',
-            },
-        ],
-    },
-    // server
-    devServer: {
-        contentBase: path.resolve(__dirname, 'dist')
-    }
+  // configuracion de entrada y salida del bundler
+  entry: {
+    bundle: './src/js/index.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Webpack App',
+      filename: 'index.html',
+      template: './src/index.html'
+    })
+  ],
+  // Estilos para la pagina
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|icon)$/i,
+        type: 'asset/resource',
+      },
+    ]
+  },
+  devServer: {
+    static: path.resolve(__dirname, 'dist'),
+    port: 3000,
+    open: true,
+    hot: true
+  }
 } 
