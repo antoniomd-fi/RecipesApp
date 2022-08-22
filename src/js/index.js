f21d4900ba56b8c98494c1c7cc44bc9254348575
VictorSosa-dev <manuelsosapia@gmail.com>
2022-08-21 21:54:20 -0500

    added images

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,26 @@
 import '../css/styles.css'
 import '../css/index.css'
+import salmonTeriyaki from '../assets/salmon-teriyaki.jpg';
+import arrabiata from '../assets/arrabiata.jpg';
+import caldpo from '../assets/caldpo.jpg';
+import appleBlackberry from '../assets/Apple-Blackberry-Crumble.jpg';
+import chocolateGateau from '../assets/Chocolate-Gateau.jpg';
+import potatoGratinChicken from '../assets/potato-gratin-chicken.jpg';
+import chikenTeriyaki from '../assets/chicken-teriyaki.jpg';
+import endesarrollo from '../assets/en-desarrollo.png';
+
+const img1 = document.getElementById('img1');
+img1.src = salmonTeriyaki;
+const img2 = document.getElementById('img2');
+img2.src = arrabiata;
+const img3 = document.getElementById('img3');
+img3.src = chikenTeriyaki;
+const img4 = document.getElementById('img4');
+img4.src = potatoGratinChicken;
+const img5 = document.getElementById('img5');
+img5.src = appleBlackberry;
+const img6 = document.getElementById('img6');
+img6.src = chocolateGateau;
+
+
+
