903c95a9e00929db743375e1f954866f8ecafd06
Antonio Martin <90221784+antoniomd-fi@users.noreply.github.com>
2022-08-24 13:17:06 -0500

    Update: index
    Add the modal and responsive design to all the page

--- src/js/index.js
+++ src/js/index.js
@@ -2,12 +2,11 @@ import '../css/styles.css'
 import '../css/index.css'
 import salmonTeriyaki from '../assets/salmon-teriyaki.jpg';
 import arrabiata from '../assets/arrabiata.jpg';
-import caldpo from '../assets/caldpo.jpg';
 import appleBlackberry from '../assets/Apple-Blackberry-Crumble.jpg';
 import chocolateGateau from '../assets/Chocolate-Gateau.jpg';
 import potatoGratinChicken from '../assets/potato-gratin-chicken.jpg';
 import chikenTeriyaki from '../assets/chicken-teriyaki.jpg';
-import endesarrollo from '../assets/en-desarrollo.png';
+import modalExampleImage from '../assets/modal-example.jpg'
 
 
 //IMAGENES PARA LA SECCIÓN DE POPULARES
@@ -24,6 +23,8 @@ const img5 = document.getElementById('img-popular-5');
 img5.src = appleBlackberry;
 const img6 = document.getElementById('img-popular-6');
 img6.src = chocolateGateau;
+const modalExample = document.getElementById('modal-image')
+modalExample.src = modalExampleImage
 
 
 
