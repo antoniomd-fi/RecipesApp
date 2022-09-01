2844678ee8f17b9511ab0218f57b4cc789837094
Antonio Martin <90221784+antoniomd-fi@users.noreply.github.com>
2022-09-01 13:28:49 -0500

    Fix: conflcts

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,10 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackFavicons = require('webpack-favicons');
 
 module.exports = {
   // configuracion de entrada y salida del bundler
   entry: {
-    index: './src/js/index.js',
-    steps: './src/js/steps.js',
-    autocomplete: './src/js/autocomplete.js'
+    bundle: './src/js/index.js'
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -18,20 +15,6 @@ module.exports = {
       title: 'Webpack App',
       filename: 'index.html',
       template: './src/index.html'
-    }),
-    new HtmlWebpackPlugin({
-      title: 'Steps',
-      filename: 'steps.html',
-      template: './src/steps.html'
-    }),
-    new WebpackFavicons({
-      src: 'assets/favicons/favicon.ico',
-      path: 'img',
-      background: '#000',
-      theme_color: '#000',
-      icons: {
-        favicons: true
-      }
     })
   ],
   // Estilos para la pagina
