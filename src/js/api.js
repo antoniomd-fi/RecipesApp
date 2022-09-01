958e217a05f7e21edbea7dd93aa721b254fc283d
VictorSosa-dev <manuelsosapia@gmail.com>
2022-09-01 02:54:17 -0500

    fix: random

--- src/js/api.js
+++ src/js/api.js
@@ -1,9 +1,11 @@
-
+window.addEventListener('load', () => {
+    localStorage.setItem('meals', '');
+    random();
+});
 // Number of results to show
 const NUMBER_OF_RESULTS = 9;
 let maxMeals;
 inicial(); // beggining of the page
-
 
 //const results = document.getElementById('results');
 const url = 'https://www.themealdb.com/api/json/v1/1/random.php';
@@ -16,7 +18,7 @@ buttonRandom.addEventListener('click', function () {
         .then(data => {
             const mealElement = document.createElement('div');
             mealElement.className = 'card card-meal m-3';
-            mealElement.style.width = '26rem';
+            mealElement.style.width = '28rem';
             const linkImg = document.createElement('a');
             linkImg.href = `steps.html?id=${data.meals[0].idMeal}`;
             const img = document.createElement('img');
@@ -103,23 +105,20 @@ function buildUrl(search) {
 }
 
 // Method fetch to get data from api
-function getMealsResults(url) {
-    return fetch(url)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            return data.meals;
-        })
-        .catch(function (err) {
-            console.log("Error >>>", err);
-        })
+async function getMealsResults(url) {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        return data.meals;
+    } catch (err) {
+        console.log("Error >>>", err);
+    }
 }
 
 // function to show meals in container
 function showMeals(meals) {
     results.textContent = '';
-
+    localStorage.setItem('meals', '');
     if (meals === null) {
         const noResults = document.createElement('div');
         noResults.innerHTML = `
@@ -163,5 +162,75 @@ function showMeals(meals) {
         );
     }
 }
+
+// function to show meals in container
+function random() {
+    results.textContent = '';
+    const meal = localStorage.getItem('meal');
+    if (meal) {
+        const meal = JSON.parse(meal);
+        const mealElement = document.createElement('div');
+        mealElement.className = 'card card-meal m-3';
+        mealElement.style.width = '28rem';
+        const linkImg = document.createElement('a');
+        linkImg.href = `steps.html?id=${meal.idMeal}`;
+        const img = document.createElement('img');
+        img.src = meal.strMealThumb;
+        img.alt = meal.strMeal;
+        const cardBody = document.createElement('div');
+        cardBody.className = 'card-body';
+        const h5 = document.createElement('h5');
+        h5.className = 'card-title';
+        h5.textContent = meal.strMeal;
+        const br = document.createElement('br');
+        const span = document.createElement('span');
+        span.className = 'read-more';
+        const a = document.createElement('a');
+        a.href = `steps.html?id=${meal.idMeal}`;
+        a.textContent = 'See the recipe';
+        mealElement.appendChild(linkImg);
+        linkImg.appendChild(img);
+        mealElement.appendChild(cardBody);
+        cardBody.appendChild(h5);
+        cardBody.appendChild(br);
+        cardBody.appendChild(span);
+        span.appendChild(a);
+        results.appendChild(mealElement);
+    }
+}
+
+
+//consumir api y mostrar modal con la receta
+const urlRandom = 'https://www.themealdb.com/api/json/v1/1/random.php'
+
+const buttonRandom2 = document.querySelector('.button-random');
+buttonRandom2.addEventListener('click', () => {
+  fetch(urlRandom)
+    .then(response => response.json())
+    .then(data => {
+      const recipe = data.meals[0];
+      console.log(recipe+'hola');
+      const recipeContent = document.getElementById('recipe-content');
+      let html = `
+      <div class="modal-header">
+      <h5 class="modal-title" id="browserModalLongTitle">${recipe.strMeal}</h5>
+      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
+      </div>
+      <div class="modal-body">
+      <img src="${recipe.strMealThumb}" alt="${recipe.strMeal}" width="50%">
+      <p>${recipe.strInstructions}</p>
+      </div>
+      <div class="modal-footer">
+      <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      <a href="${recipe.strYoutube}" target = "_blank" class="btn btn-primary modal-continue-button">See steps</a>
+      </div>
+      `;
+    recipeContent.innerHTML = html;
+    })
+    .catch(error => console.log(error));
+});
+
 
 export { inicial }; // beggining of the page
