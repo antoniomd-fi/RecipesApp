389c4a4e6362808cb43afa0814207661e0503637
Antonio Martin <90221784+antoniomd-fi@users.noreply.github.com>
2022-09-01 13:29:47 -0500

    Fix: conflicts

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,9 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   // configuracion de entrada y salida del bundler
   entry: {
-    bundle: './src/js/index.js'
+    index: './src/js/index.js',
+    random: './src/js/random.js',
+    steps: './src/js/steps.js'
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -15,7 +17,17 @@ module.exports = {
       title: 'Webpack App',
       filename: 'index.html',
       template: './src/index.html'
-    })
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Steps',
+      filename: 'steps.html',
+      template: './src/steps.html'
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Random',
+      filename: 'random.html',
+      template: './src/random.html'
+    }),
   ],
   // Estilos para la pagina
   module: {
